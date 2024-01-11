5e6f7df2da5821183ec41a76345eb047de5240e9
Denis Snegirev <snegirev.dv@gmail.com>
2024-01-11 15:01:10 +0300

    Linting

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,16 @@
 import argparse
 
-def main():
+
+def main() -> None:
     parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.'
+        description="Compares two configuration files and shows a difference."
     )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    args = parser.parse_args()
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    args: argparse.Namespace = parser.parse_args()
     print("Первый файл:", args.first_file)
     print("Второй файл:", args.second_file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
