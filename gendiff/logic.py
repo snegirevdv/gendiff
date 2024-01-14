d6eafd01662193e3eaea0e41d71606efc9bbc6a7
“Denis <snegirev.dv@gmail.com>
2024-01-14 13:32:11 +0300

    Add yaml support

--- gendiff/logic.py
+++ gendiff/logic.py
@@ -1,5 +1,8 @@
 import json
+from collections.abc import Callable, Hashable
 from typing import Any
+
+import yaml
 
 JSON_VALUE_CONVERTER = {
     True: "true",
@@ -8,30 +11,57 @@ JSON_VALUE_CONVERTER = {
 }
 
 LINE_PREFIX_VALUES = {
-    -1: "  - ",
-    0: "    ",
-    1: "  + ",
+    -1: "-",
+    0: " ",
+    1: "+",
 }
 
 
 def generate_diff(file1: str, file2: str) -> str:
+    dict1, dict2 = load_data(file1, file2)
+    merged_data = merge_dicts(dict1, dict2)
+    return get_diff_report(merged_data)
+
+
+def load_data(file1: str, file2: str) -> Callable:
     try:
-        with open(file1) as file1, open(file2) as file2:
-            try:
-                json1: dict = json.load(file1)
-                json2: dict = json.load(file2)
-            except json.JSONDecodeError:
-                raise ValueError("ERROR: Files contain invalid data")
+        if file1.endswith('json') and file2.endswith('json'):
+            return load_json(file1, file2)
+        if file1.endswith(('yml', 'yaml')) and file2.endswith(('yml', 'yaml')):
+            return load_yaml(file1, file2)
+        raise ValueError("ERROR: Files have incorrect extension")
     except FileNotFoundError:
         raise FileNotFoundError("ERROR: File Not Found")
-    merged_jsons = merge_dicts(json1, json2)
-    return get_diff_report(merged_jsons)
+
+
+def load_json(
+    file1: str,
+    file2: str,
+) -> tuple[dict[Hashable, Any], dict[Hashable, Any]]:
+    with open(file1) as file1, open(file2) as file2:
+        try:
+            return (json.load(file1), json.load(file2))
+        except json.JSONDecodeError:
+            raise ValueError("ERROR: Files contain invalid data")
+
+
+def load_yaml(
+    file1: str,
+    file2: str,
+) -> tuple[dict[Hashable, Any], dict[Hashable, Any]]:
+    with open(file1) as file1, open(file2) as file2:
+        try:
+            dict1 = yaml.safe_load(file1) or {}
+            dict2 = yaml.safe_load(file2) or {}
+            return (dict1, dict2)
+        except (yaml.YAMLError, KeyError):
+            raise ValueError("ERROR: Files contain invalid data")
 
 
 def merge_dicts(
     dict1: dict[str, Any],
     dict2: dict[str, Any],
-) -> dict[tuple, Any]:
+) -> dict[tuple[Hashable, int], Any]:
     merged = {}
 
     keys_1 = set(dict1)
@@ -66,7 +96,7 @@ def get_diff_report(data: dict) -> str:
     result = "{\n"
     for key_tuple, value in sorted(data.items()):
         key, prefix_key = key_tuple
-        line = f"{LINE_PREFIX_VALUES[prefix_key]}{key}: {value}\n"
+        line = f"  {LINE_PREFIX_VALUES[prefix_key]} {key}: {value}\n"
         result += line
     result += "}"
     return result
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -5,30 +5,47 @@ import pytest
 @pytest.mark.parametrize(
     "answer, file1, file2",
     [
-        ["answers/plain.txt", "jsons/plain1.json", "jsons/plain2.json"],
-        ["answers/empty.txt", "jsons/empty.json", "jsons/empty.json"],
-        ["answers/plain_empty.txt", "jsons/plain1.json", "jsons/empty.json"],
+        ["answers/plain.txt", "json/plain1.json", "json/plain2.json"],
+        ["answers/empty.txt", "json/empty.json", "json/empty.json"],
+        ["answers/plain_empty.txt", "json/plain1.json", "json/empty.json"],
+        ["answers/plain.txt", "yaml/plain1.yml", "yaml/plain2.yml"],
+        ["answers/empty.txt", "yaml/empty.yaml", "yaml/empty.yaml"],
+        ["answers/plain_empty.txt", "yaml/plain1.yml", "yaml/empty.yaml"],
     ]
 )
 def test_plain(get_answer, get_fixture_path, answer, file1, file2):
     answer_path = get_fixture_path(answer)
     file1 = get_fixture_path(file1)
     file2 = get_fixture_path(file2)
-
     answer = get_answer(answer_path)
-
     assert gendiff.generate_diff(file1, file2) == answer
 
 
 def test_not_found_error(get_fixture_path):
-    file1 = get_fixture_path("jsons/plain1.json")
-    file2 = get_fixture_path("jsons/plain2.jsn")
+    file1 = get_fixture_path("json/plain1.json")
+    file2 = get_fixture_path("json/plain.json")
     with pytest.raises(FileNotFoundError):
         gendiff.generate_diff(file1, file2)
 
 
-def test_json_error(get_fixture_path):
-    file1 = get_fixture_path("jsons/plain1.json")
-    file2 = get_fixture_path("jsons/invalid.json")
-    with pytest.raises(ValueError):
+def test_incorrect_files(get_fixture_path):
+    file1 = get_fixture_path("incorrect_format_1.png")
+    file2 = get_fixture_path("incorrect_format_2.png")
+    with pytest.raises(ValueError) as e:
         gendiff.generate_diff(file1, file2)
+    assert str(e.value) == "ERROR: Files have incorrect extension"
+
+
+@pytest.mark.parametrize(
+    "file1, file2",
+    [
+        ["json/plain1.json", "json/invalid.json"],
+        ["yaml/plain1.yml", "yaml/invalid.yml"],
+    ]
+)
+def test_encoder_error(get_fixture_path, file1, file2):
+    file1 = get_fixture_path(file1)
+    file2 = get_fixture_path(file2)
+    with pytest.raises(ValueError) as e:
+        gendiff.generate_diff(file1, file2)
+    assert str(e.value) == "ERROR: Files contain invalid data"
