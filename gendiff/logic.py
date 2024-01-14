de501080bf130f172571a4f5c412a2c87a4da084
“Denis <snegirev.dv@gmail.com>
2024-01-14 15:33:53 +0300

    Update logic.py

--- gendiff/logic.py
+++ gendiff/logic.py
@@ -1,55 +1,55 @@
+import itertools
 from collections.abc import Hashable
 from typing import Any
 
 from gendiff import parser
 
-LINE_PREFIX_VALUES = {
-    -1: "-",
-    0: " ",
-    1: "+",
+LINE_PREFIXES = {
+    "deleted": "-",
+    "unchanged": " ",
+    "added": "+",
 }
 
 
 def generate_diff(file1: str, file2: str) -> str:
     dict1, dict2 = parser.parse_data(file1, file2)
-    merged_data = merge_dicts(dict1, dict2)
-    return get_diff_report(merged_data)
+    diff = make_diff(dict1, dict2)
+    return get_diff_report(diff, dict1, dict2)
 
 
-def merge_dicts(
+def make_diff(
     dict1: dict[str, Any],
     dict2: dict[str, Any],
 ) -> dict[tuple[Hashable, int], Any]:
-    merged = {}
     keys_1 = set(dict1)
     keys_2 = set(dict2)
 
-    unchanged_keys = set(
-        filter(lambda key: dict1[key] == dict2[key], keys_1 & keys_2)
-    )
-    changed_keys = set(
-        filter(lambda key: dict1[key] != dict2[key], keys_1 & keys_2)
-    )
-    deleted_keys = keys_1 - keys_2
-    added_keys = keys_2 - keys_1
+    unchanged = dict(zip(
+        filter(lambda key: dict1[key] == dict2[key], keys_1 & keys_2),
+        itertools.repeat("unchanged")
+    ))
+    changed = dict(zip(
+        filter(lambda key: dict1[key] != dict2[key], keys_1 & keys_2),
+        itertools.repeat("changed")
+    ))
+    deleted = dict(zip(keys_1 - keys_2, itertools.repeat("deleted")))
+    added = dict(zip(keys_2 - keys_1, itertools.repeat("added")))
 
-    for key in unchanged_keys:
-        merged[(key, 0)] = dict1[key]
-
-    for key in deleted_keys | changed_keys:
-        merged[(key, -1)] = dict1[key]
-
-    for key in added_keys | changed_keys:
-        merged[(key, 1)] = dict2[key]
-
-    return merged
+    return unchanged | changed | deleted | added
 
 
-def get_diff_report(data: dict) -> str:
+def get_diff_report(diff: dict, dict1: dict, dict2: dict) -> str:
     result = "{\n"
-    for key_tuple, value in sorted(data.items()):
-        key, prefix_key = key_tuple
-        line = f"  {LINE_PREFIX_VALUES[prefix_key]} {key}: {value}\n"
-        result += line
+    for key, difference in sorted(diff.items()):
+        if difference in ("deleted", "changed"):
+            result += get_line(LINE_PREFIXES["deleted"], key, dict1)
+        if difference in ("added", "changed"):
+            result += get_line(LINE_PREFIXES["added"], key, dict2)
+        if difference == "unchanged":
+            result += get_line(LINE_PREFIXES["unchanged"], key, dict1)
     result += "}"
     return result
+
+
+def get_line(prefix: str, key: Hashable, source: dict[Hashable, Any]):
+    return f"  {prefix} {key}: {source[key]}\n"
